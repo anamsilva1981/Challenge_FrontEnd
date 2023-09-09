a37eaf5dcacaf24b2d82759aaf997acd73afa030
ana-msilva <ana.msilva@ibm.com>
2023-09-09 20:37:44 -0300

    feat: add install primeNG

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private primengConfig = inject(PrimeNGConfig);
+  private config = inject(PrimeNGConfig);
+
   title = 'frontend';
+  
+  ngOnInit() {
+    this.ripple();
+    this.zIndex();
+    this.filterMatchModeOptions();
+  }
+
+  public ripple(){
+    this.primengConfig.ripple = true;
+  }
+
+  public zIndex(){
+    this.primengConfig.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+    };
+  }
+
+  public filterMatchModeOptions(){
+    this.primengConfig.filterMatchModeOptions = {
+      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
+      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
+      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
+      }
+  };
+
 }
