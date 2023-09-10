ab78668deab470168d142cb182aa89aef7b4d6f2
ana-msilva <ana.msilva@ibm.com>
2023-09-10 15:00:48 -0300

    feat: add footer

--- frontend/src/app/layout/layout.module.ts
+++ frontend/src/app/layout/layout.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { PrimeNGModule } from '../prime-ng/prime-ng.module';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     PrimeNGModule,
     FormsModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
